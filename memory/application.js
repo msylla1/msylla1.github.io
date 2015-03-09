3459a9ab4e85c42f03d9b2d3609269ac79b36f8c
msylla1 <msylla@kippnyccp.org>
2015-03-09 13:33:40 -0400

    Update application.js

--- memory/application.js
+++ memory/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	var my_array = ["1","1","2","2","3","3","4","4","5","6","6","7","7"];
+	var my_array = ["1","1","2","2","3","3","4","4","5","5","6","6","7","7"];
   for(i in my_array){
     $('#card_holder').append('<div class="card"><p>'+my_array[i]+'</p></div>');
   };
