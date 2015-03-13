e96a59862cbd36d653455f747d76008cf90410b7
msylla1 <msylla@kippnyccp.org>
2015-03-13 13:29:45 -0400

    Update application.js

--- memory/application.js
+++ memory/application.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   var my_array = ["1","1","2","2","3","3","4","4","5","5","6","6","7","7","8","8","9","9","10","10"];
   function shuffle(array) {
     var counter = array.length, temp, index;
-
+  //This gives the list of cards that are to appear.
     // While there are elements in the array
     while (counter > 0) {
         // Pick a random index
@@ -32,8 +32,10 @@ $(document).ready(function(){
     $(this).find('p').addClass('clicked');
     first_click = $(this).find('p').html();
     click_count = 1;
+    //This tells us what should happen if two of the cards matches
     }
     else {
+      //Tells us that the cards should turn aruond if the cards don't match.
     $(this).find('p').css("opacity", 1);
     $(this).find('p').addClass('clicked');
     second_click = $(this).find('p').html();
