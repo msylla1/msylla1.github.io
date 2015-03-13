22cfd15b08f649d407847a090274e59e47d7c2f7
msylla1 <msylla@kippnyccp.org>
2015-03-13 13:15:35 -0400

    Update application.js

--- memory/application.js
+++ memory/application.js
@@ -23,9 +23,26 @@ var new_array = shuffle(my_array);
   for(i in new_array){
     $('#card_holder').append('<div class="card"><p>'+my_array[i]+'</p></div>');
   };
-	$('.card').click(function(){
-		$(this).find("p").css("opacity", 1);
-		$(this).html();
-  
-	});
-  });
+$('.card').click(function(){
+    if(click_count == 0){
+    $(this).find('p').css("opacity", 1);
+    $(this).find('p').addClass('clicked');
+    first_click = $(this).find('p').html();
+    click_count = 1;
+    }
+    else {
+    $(this).find('p').css("opacity", 1);
+    $(this).find('p').addClass('clicked');
+    second_click = $(this).find('p').html();
+    click_count = 0;
+      if (first_click == second_click){
+        $('.clicked').css("opacity", 1).removeClass('clicked');
+      }
+      else {
+        setTimeout(function(){$('.clicked').css('opacity', '0').removeClass('clicked')}, 500);
+      }
+      click_count = 0;
+    }
+    
+  })
+})
