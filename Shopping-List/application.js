f4cdd91a863706cc7841d09a0164902c1e783a73
msylla1 <msylla@kippnyccp.org>
2015-03-20 13:40:07 -0400

    Update application.js

--- Shopping-List/application.js
+++ Shopping-List/application.js
@@ -1,1 +1,7 @@
-
+$(document).ready(function(){
+$(#'add').on("click",function(){
+  $('input'). val();
+  var value=$('input').val()
+$('#1').append("<p>"+value+"</p>")
+  });
+$('#clear').on("click", function(){
