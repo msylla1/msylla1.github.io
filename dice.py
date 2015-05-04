b26c6c30d85ac2f0f93ededdebebc89643dff947
msylla1 <msylla@kippnyccp.org>
2015-05-04 13:33:14 -0400

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,9 +1,10 @@
 from random import randint
 num={2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0}
-pro=roll/500
+#pro=roll/500
 for i in range(500):
    roll=randint(1,6)+randint(1,6)
    num[roll] += 1
    
 for item in num:
+   pro = num[item]/500
    print pro
