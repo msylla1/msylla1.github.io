1be15fe6ca8cf046acdb234586f52db454dc217d
msylla1 <msylla@kippnyccp.org>
2015-05-12 13:23:27 -0400

    Update election.py

--- election.py
+++ election.py
@@ -1,5 +1,14 @@
 from random import randint
 
 wins=0
-for wins in range(0, 10,0000):
-  print "You won!"
+for wins in range(10000):
+  regions=0
+  if randint(1,100)<= 87:
+    regions+=1
+  if randint(1,100)<= 65:
+    regions+=1
+  if randint(1,100)<= 17:
+    regions+=1
+  if regions == 2 or regions == 3:
+    wins+=1
+print "the chances of winning are: %s" %(wins/float(10000))
